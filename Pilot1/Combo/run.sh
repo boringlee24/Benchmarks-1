b2aeb6d6c8f3c5df712436bfd9c7ded70e25195e
Baolin <boringlee24@gmail.com>
2021-04-03 19:55:11 +0000

    tweak

--- Pilot1/Combo/run.sh
+++ Pilot1/Combo/run.sh
@@ -4,5 +4,6 @@ TESTCASE=$1  #"P100_noshuffle_resnet152"
 
 python candle_inf.py --testcase $TESTCASE &&
 python resnet_inf.py --testcase $TESTCASE &&
-python vgg_inf.py --testcase $TESTCASE
+python vgg_inf.py --testcase $TESTCASE &&
+./git.sh 
 
